24398c4f7a08d6ed991b0c4dec419a233552653d
Brent Gaines <84875271+brentocracy@users.noreply.github.com>
2021-09-22 11:25:49 -0400

    installed dependencies, set up file structure, and pseudo-code

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+/*
+
+* add validation to user input for anything in need of formatting, ie:
+- for role, it must be one of the 3
+- email validator
+- office number must be a number
+- name must be a string
+- id must be a number
+
+*/
